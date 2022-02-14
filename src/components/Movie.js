9d6b965ba949bd54c423f418856c1da4e182359d
disciplechoi <poopoocupcake1991@gmail.com>
2022-02-14 22:15:39 +0900

    style

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,21 +1,26 @@
 import PropTypes from "prop-types";
 import {Link} from 'react-router-dom';
 import styles from './Movie.module.css';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+
 
 
 function Movie({id, medium_cover_image, title, summary, genres,rating})
-{
+{   
+    
     return(
     <div className={styles.gallery}>      
         <img src={medium_cover_image} className={styles.gallery__img}/>
         <Link to={`/movie/${id}`}>
         <div className={styles.detail}>
-            <h2 className={styles.movie__title, styles.hide}>{title}</h2>
-            {genres.map((g)=> (<li key={g}>{g}</li>))}
-            <p className={styles.movie__summary, styles.hide}>{rating}</p>
-            <ul className={styles.movie__genres, styles.hide}>
-            
+            <ul className={`${styles.movie__genres} ${styles.hide}`}>
+                {genres.slice(0,2).map((g)=> (<li key={g} className={styles.movie__genres__detail}>{g}</li>))}
             </ul>
+            
+            <h2 className={styles.movie__title, styles.hide}>{title}</h2>
+            
+              <Rating name="read-only" value={rating/5} readOnly />
         </div>
         </Link>
     </div>
