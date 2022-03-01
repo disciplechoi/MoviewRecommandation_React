4cdbf1a349defe960e1cdeb3b57479d69b94cfa1
disciplechoi <poopoocupcake1991@gmail.com>
2022-03-01 13:39:25 +0900

    frame 4 pictures

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,13 @@
 import Movie from "../components/Movie";
 import {useEffect, useState} from "react";
 import styles from "./Home.module.css";
+import { style } from '@mui/system';
 
 function Home(){
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
     const getMovies = async() => {
-    const response = await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year');
+    const response = await fetch('https://yts.mx/api/v2/list_movies.json?limit=50&page=2');
     const json = await response.json();
     setMovies(json.data.movies);
     setLoading(false);
@@ -21,9 +22,12 @@ function Home(){
     <div className={styles.container}>
       {loading? <h1>Loading...</h1> : 
       <div className={styles.subcontainer}>
-        <h1 className={`${styles.title__number} ${styles.title__pink}`}>100</h1>
-        <h1 className={styles.title}>GOOD MOVIES</h1>
-        <h1 className={`${styles.subtitle} ${styles.title__pink}`}>FOR YOU</h1>
+        <div className={styles.title}>
+          <h1 className={`${styles.title__number} ${styles.title__pink}`}>100</h1>
+          <h1>GOOD MOVIES</h1>
+        </div>
+        
+     
             <div className={styles.gallery}>
             {movies.map((movie=> 
             <Movie 
